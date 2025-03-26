02eaffd224586175f532e98039d85d8e19ea308b
KhanhDCQ <khanhdcq@fpt.com>
2025-03-26 23:23:14 +0700

    create MilkTeaProductServices

---
+++ PRN222.Assignment.Services/Product/IMilkTeaProductService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PRN222.Assignment.Repositories.Entities;
+
+namespace PRN222.Assignment.Services.Product
+{
+    public interface IMilkTeaProductService
+    {
+        List<MilkTeaProduct> GetMilkTeaProducts();
+        MilkTeaProduct GetMilkTeaProductById(int id);
+        void AddMilkTeaProduct(MilkTeaProduct milkTeaProduct);
+        void UpdateMilkTeaProduct(MilkTeaProduct milkTeaProduct);
+        void DeleteMilkTeaProduct(MilkTeaProduct milkTeaProduct);
+
+    }
+}
---
+++ PRN222.Assignment.Services/Product/MilkTeaProductService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PRN222.Assignment.Repositories.Entities;
+using PRN222.Assignment.Repositories.Repositories.Interface;
+
+namespace PRN222.Assignment.Services.Product
+{
+    public class MilkTeaProductService : IMilkTeaProductService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MilkTeaProductService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void AddMilkTeaProduct(MilkTeaProduct milkTeaProduct)
+        {
+            _unitOfWork.MilkTeaProducts.Add(milkTeaProduct);
+            _unitOfWork.SaveAsync();
+        }
+
+        public void DeleteMilkTeaProduct(MilkTeaProduct milkTeaProduct)
+        {
+            _unitOfWork.MilkTeaProducts.Delete(milkTeaProduct);
+            _unitOfWork.SaveAsync();
+        }
+
+        public MilkTeaProduct GetMilkTeaProductById(int id)
+        {
+            return _unitOfWork.MilkTeaProducts.GetById(id);
+        }
+
+        public List<MilkTeaProduct> GetMilkTeaProducts()
+        {
+            return _unitOfWork.MilkTeaProducts.GetAll().ToList();
+        }
+
+        public void UpdateMilkTeaProduct(MilkTeaProduct milkTeaProduct)
+        {
+             _unitOfWork.MilkTeaProducts.Update(milkTeaProduct);
+            _unitOfWork.SaveAsync();
+        }
+    }
+}
