ca98dfb33c599b1d0727ef29fd348653dc745590
Duyen9904 <mimitrucduyen@gmail.com>
2025-03-26 16:13:39 +0700

    update generic repo

--- PRN222.Assignment.Repositories/Repositories/GenericRepository.cs
+++ PRN222.Assignment.Repositories/Repositories/GenericRepository.cs
@@ -1,60 +1,132 @@
-﻿using Microsoft.EntityFrameworkCore;
-using PRN222.Assignment.Repositories.Repositories.Interface;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PRN222.Assignment.Repositories.Repositories.Interface;
 
 namespace PRN222.Assignment.Repositories.Repositories
 {
     public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
-        private readonly DbContext _context;
-        private readonly DbSet<T> _dbSet;
+        protected readonly DbContext _context;
+        protected readonly DbSet<T> _dbSet;
 
         public GenericRepository(DbContext context)
         {
             _context = context;
-            _dbSet = _context.Set<T>();
+            _dbSet = context.Set<T>();
         }
 
-        public async Task<(List<T> Items, int TotalCount)> GetAsync(
-            Expression<Func<T, bool>>? filter = null,
-            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
-            int? page = null,
+        #region Synchronous Methods
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            int? pageIndex = null,
             int? pageSize = null,
-            Func<IQueryable<T>, IQueryable<T>>? include = null)
+            params Expression<Func<T, object>>[] includes)
         {
             IQueryable<T> query = _dbSet;
 
-            if (include != null)
+            // Apply includes for eager loading
+            if (includes != null)
             {
-                query = include(query);
+                query = includes.Aggregate(query, (current, include) => current.Include(include));
             }
 
+            // Apply filter
             if (filter != null)
             {
                 query = query.Where(filter);
             }
 
-            int totalCount = await query.CountAsync();
-
+            // Apply ordering
             if (orderBy != null)
             {
                 query = orderBy(query);
             }
 
-            if (page.HasValue && pageSize.HasValue)
+            // Apply pagination
+            if (pageIndex.HasValue && pageSize.HasValue)
             {
-                query = query.Skip((page.Value - 1) * pageSize.Value).Take(pageSize.Value);
+                query = query.Skip((pageIndex.Value - 1) * pageSize.Value).Take(pageSize.Value);
             }
 
-            return (await query.ToListAsync(), totalCount);
+            return query.ToList();
         }
 
-        public async Task<T?> GetByIdAsync(int id)
+        public T GetById(object id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(T entity)
+        {
+            if (_context.Entry(entity).State == EntityState.Detached)
+            {
+                _dbSet.Attach(entity);
+            }
+            _dbSet.Remove(entity);
+        }
+
+        public List<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate).ToList();
+        }
+
+        #endregion
+
+        #region Asynchronous Methods
+
+        public async Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            int? pageIndex = null,
+            int? pageSize = null,
+            params Expression<Func<T, object>>[] includes)
+        {
+            IQueryable<T> query = _dbSet;
+
+            // Apply includes for eager loading
+            if (includes != null)
+            {
+                query = includes.Aggregate(query, (current, include) => current.Include(include));
+            }
+
+            // Apply filter
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            // Apply ordering
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+
+            // Apply pagination
+            if (pageIndex.HasValue && pageSize.HasValue)
+            {
+                query = query.Skip((pageIndex.Value - 1) * pageSize.Value).Take(pageSize.Value);
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(object id)
         {
             return await _dbSet.FindAsync(id);
         }
@@ -62,24 +134,30 @@ namespace PRN222.Assignment.Repositories.Repositories
         public async Task AddAsync(T entity)
         {
             await _dbSet.AddAsync(entity);
-            await _context.SaveChangesAsync();
         }
 
-        public void Update(T entity)
+        public async Task UpdateAsync(T entity)
         {
-            _dbSet.Update(entity);
-            _context.SaveChanges();
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            await Task.CompletedTask; // To make the method async compatible
         }
 
-        public void Delete(T entity)
+        public async Task DeleteAsync(T entity)
         {
+            if (_context.Entry(entity).State == EntityState.Detached)
+            {
+                _dbSet.Attach(entity);
+            }
             _dbSet.Remove(entity);
-            _context.SaveChanges();
+            await Task.CompletedTask; // To make the method async compatible
         }
 
         public async Task<List<T>> FindAsync(Expression<Func<T, bool>> predicate)
         {
             return await _dbSet.Where(predicate).ToListAsync();
         }
+
+        #endregion
     }
 }
--- PRN222.Assignment.Repositories/Repositories/Interface/IGenericRepository.cs
+++ PRN222.Assignment.Repositories/Repositories/Interface/IGenericRepository.cs
@@ -9,18 +9,33 @@ namespace PRN222.Assignment.Repositories.Repositories.Interface
 {
     public interface IGenericRepository<T> where T : class
     {
-        Task<(List<T> Items, int TotalCount)> GetAsync(
-            Expression<Func<T, bool>>? filter = null,
-            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
-            int? page = null,
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            int? pageIndex = null,
             int? pageSize = null,
-            Func<IQueryable<T>, IQueryable<T>>? include = null
-        );
+            params Expression<Func<T, object>>[] includes);
 
-        Task<T?> GetByIdAsync(int id);
-        Task AddAsync(T entity);
+        T GetById(object id);
+
+        void Add(T entity);
+
         void Update(T entity);
         void Delete(T entity);
+
+        List<T> Find(Expression<Func<T, bool>> predicate);
+
+        Task<IEnumerable<T>> GetAllAsync(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            int? pageIndex = null,
+            int? pageSize = null,
+            params Expression<Func<T, object>>[] includes);
+        Task<T> GetByIdAsync(object id);
+
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
         Task<List<T>> FindAsync(Expression<Func<T, bool>> predicate);
     }
 }
