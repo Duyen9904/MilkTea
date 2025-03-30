f3a27a09701a1ec2d48a73515b4ac924d3d59879
hoanglhse172418 <hoanglhse172418@fpt.edu.vn>
2025-03-30 12:31:29 +0700

    update nav menu

---
+++ PRN222.Assignment.Razor.Admin/Components/ComboOrder/Create.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PRN222.Assignment.Repositories.Entities;
+using PRN222.Assignment.Services.ComboOrders;
+
+namespace PRN222.Assignment.Razor.Admin.Pages.ComboOrder
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IComboOrderService _comboOrderService;
+        private readonly MilkTeaShopContext _context;
+
+        public CreateModel(IComboOrderService comboOrderService, MilkTeaShopContext context)
+        {
+            _comboOrderService = comboOrderService;
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            ViewData["ComboId"] = new SelectList(_context.Combos, "ComboId", "ComboName");
+            ViewData["OrderId"] = new SelectList(_context.Orders, "OrderId", "DeliveryAddress");
+            return Page();
+        }
+
+        [BindProperty]
+        public OrderCombo OrderCombo { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _comboOrderService.AddOrderCombo(OrderCombo);
+            await Task.CompletedTask;
+            return RedirectToPage("./Index");
+        }
+    }
+
+}
---
+++ PRN222.Assignment.Razor.Admin/Components/ComboOrder/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PRN222.Assignment.Repositories.Entities;
+using PRN222.Assignment.Services.ComboOrders;
+
+namespace PRN222.Assignment.Razor.Admin.Pages.ComboOrder
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly IComboOrderService _comboOrderService;
+
+        public DeleteModel(IComboOrderService comboOrderService)
+        {
+            _comboOrderService = comboOrderService;
+        }
+
+        [BindProperty]
+        public OrderCombo OrderCombo { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            OrderCombo = _comboOrderService.GetOrderComboById(id.Value);  // Use service to get the OrderCombo by ID
+
+            if (OrderCombo == null)
+            {
+                return NotFound();
+            }
+            await Task.CompletedTask;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            OrderCombo = _comboOrderService.GetOrderComboById(id.Value);
+
+            if (OrderCombo != null)
+            {
+                _comboOrderService.DeleteOrderCombo(OrderCombo);  // Use service to delete the OrderCombo
+            }
+            await Task.CompletedTask;
+            return RedirectToPage("./Index");
+        }
+    }
+}
