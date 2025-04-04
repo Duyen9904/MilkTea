5a0d0321143f656080585eda8a5be63fc82a0a48
tykyphatky <gg.fctaiphat@yahoo.com>
2025-04-04 14:59:18 +0700

    enhance ui v1

---
+++ PRN222.Assignment.Repositories/Entities/MilkTeaProductView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN222.Assignment.Repositories.Entities
+{
+    public class MilkTeaProductView
+    {
+        public MilkTeaProduct Product { get; set; }
+        //public IFormFile? ImageFile { get; set; }
+    }
+}
---
+++ PRN222.Assignment.Services/Implementations/SupabaseStorageService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Supabase;
+using Supabase.Gotrue;
+using Supabase.Postgrest;
+
+
+namespace PRN222.Assignment.Services.Implementations
+{
+    public class SupabaseStorageService
+    {
+        private readonly Supabase.Client _supabaseClient;
+        private readonly string _bucketName = "milktea-images";
+
+        public SupabaseStorageService(IConfiguration configuration)
+        {
+            var url = configuration["Supabase:Url"];
+            var key = configuration["Supabase:Key"];
+            _supabaseClient = new Supabase.Client(url, key);
+
+            InitializeBucketAsync().GetAwaiter().GetResult();
+        }
+
+        private async Task InitializeBucketAsync()
+        {
+            var buckets = await _supabaseClient.Storage.ListBuckets();
+            if (!buckets.Any(b => b.Name == _bucketName))
+            {
+                await _supabaseClient.Storage.CreateBucket(_bucketName);
+            }
+        }
+
+        //public async Task<string> UploadProductImageAsync(Stream fileStream, string fileName, string contentType)
+        //{
+        //    var uniqueFileName = $"{DateTime.Now:yyyyMMddHHmmss}_{Guid.NewGuid()}_{fileName}";
+
+        //    await _supabaseClient.Storage
+        //        .From(_bucketName)
+        //        .Upload(fileStream, uniqueFileName, new Supabase.Storage.UploadOptions
+        //        {
+        //            ContentType = contentType,
+        //            Upsert = true 
+        //        });
+
+        //    return GetImageUrl(uniqueFileName);
+        //}
+
+        public string GetImageUrl(string filePath)
+        {
+            return _supabaseClient.Storage
+                .From(_bucketName)
+                .GetPublicUrl(filePath);
+        }
+
+        public async Task DeleteImageAsync(string filePath)
+        {
+            string fileName = Path.GetFileName(filePath);
+
+            await _supabaseClient.Storage
+                .From(_bucketName)
+                .Remove(new List<string> { fileName });
+        }
+    }
+}
